e41c2686de1cf3abf0aac61d6fcea56a548bd07e
Azamat Isakov <i.azamat.isakov@gmail.com>
2026-01-08 03:20:28 +0500

    Add ant-design-vue

--- app/app.vue
+++ app/app.vue
@@ -1,6 +1,15 @@
+<script setup>
+import { SearchOutlined } from "@ant-design/icons-vue";
+</script>
+
 <template>
-  <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+  <div style="padding: 50px">
+    <h1>Проверка Ant Design Module</h1>
+    <!-- Используем компоненты напрямую, без import -->
+    <a-space>
+      <a-button type="primary">Primary Button</a-button>
+      <a-button>Default Button</a-button>
+      <a-tag color="success">Success Tag</a-tag>
+    </a-space>
   </div>
 </template>
